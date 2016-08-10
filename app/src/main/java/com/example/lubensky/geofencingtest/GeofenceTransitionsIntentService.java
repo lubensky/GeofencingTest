145c29b31f51935271e1b8b249bf932720509df9
lubensky <Hd7Bbd7Z>
2016-08-10 23:38:45 +0200

    Geofencing running

--- app/src/main/java/com/example/lubensky/geofencingtest/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/example/lubensky/geofencingtest/GeofenceTransitionsIntentService.java
@@ -1,7 +1,13 @@
 package com.example.lubensky.geofencingtest;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 /**
  * Created by lubensky on 10.08.16.
@@ -16,6 +22,23 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent( intent);
 
+        if( geofencingEvent.hasError()) {
+            Log.e( TAG, "Geofencing error");
+
+            return;
+        }
+
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+        Vibrator v = ( Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        if( geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            v.vibrate( MainActivity.VIBRATE_DURATION_ENTER);
+        } else if ( geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+
+        } else if ( geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            v.vibrate( MainActivity.VIBRATE_DURATION_EXIT);
+        }
     }
 }
--- app/src/main/java/com/example/lubensky/geofencingtest/MainActivity.java
+++ app/src/main/java/com/example/lubensky/geofencingtest/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.lubensky.geofencingtest;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, // for GoogleApiClient.Builder.addConnectionCallback
-        GoogleApiClient.OnConnectionFailedListener { // for GoogleApiClient.Builder.addOnConnectionFailedListener
+        GoogleApiClient.OnConnectionFailedListener, // for GoogleApiClient.Builder.addOnConnectionFailedListener
+        ResultCallback<Status> { // for GeofencingApi.setResultCallback
+    // home
+    static final private LatLng POINT_OF_INTEREST = new LatLng( 51.071584, 13.731136);
+    static final private int RADIUS = 50;
+    static final public int VIBRATE_DURATION_ENTER = 10000;
+    static final public int VIBRATE_DURATION_EXIT = 5000;
+    static final private int LOITERING_DELAY = 1000;
+    static final private String TAG = "MainActivity";
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -35,10 +52,54 @@ public class MainActivity extends AppCompatActivity implements
         client = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API) 
+                .addApi(LocationServices.API)
                 .build();
     }
 
+    protected void addGeofences() {
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                    client,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()).setResultCallback( this);
+        } catch( SecurityException securityException) {
+            logSecurityException( securityException);
+        }
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        Intent intent = new Intent( this, GeofenceTransitionsIntentService.class);
+
+        return PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        builder.setInitialTrigger( GeofencingRequest.INITIAL_TRIGGER_ENTER);
+
+        Geofence geofence = new Geofence.Builder()
+                .setRequestId( "POINT_OF_INTEREST")
+                .setCircularRegion(
+                        POINT_OF_INTEREST.latitude,
+                        POINT_OF_INTEREST.longitude,
+                        RADIUS)
+                .setTransitionTypes(
+                        Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT |
+                                    Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay( LOITERING_DELAY)
+                .setExpirationDuration( Geofence.NEVER_EXPIRE)
+                .build();
+
+        builder.addGeofence( geofence);
+
+        return builder.build();
+    }
+
+    private void logSecurityException(SecurityException securityException) {
+        Log.e(TAG, "Invalid location permission. " +
+                "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
+    }
 
     @Override
     public void onStart() {
@@ -56,7 +117,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        addGeofences();
     }
 
     @Override
@@ -66,6 +127,11 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
 
     }
 }
